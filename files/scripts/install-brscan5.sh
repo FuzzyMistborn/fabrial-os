22298312fe785155de6e3ff530770a6eebcd692a
FuzzyMistborn <fuzzy@fuzzymistborn.com>
2026-04-30 18:47:57 -0400

    Find instead of ls

--- files/scripts/install-brscan5.sh
+++ files/scripts/install-brscan5.sh
@@ -6,9 +6,9 @@ curl -L -o /tmp/brscan5.rpm https://download.brother.com/welcome/dlf104036/brsca
 rpm -i --nodigest --nosignature /tmp/brscan5.rpm
 rm /tmp/brscan5.rpm
 
-# Fix udev rules so the scanner is accessible to the scanner group
-# (lines are commented out by default in Brother's package)
-UDEV_RULES=$(ls /etc/udev/rules.d/60-brother-mfp-brscan5-*.rules 2>/dev/null | head -1)
+# Fix udev rules so the scanner is accessible to the scanner group.
+# Brother ships these lines commented out by default.
+UDEV_RULES=$(find /etc/udev/rules.d/ -name "60-brother-mfp-brscan5-*.rules" 2>/dev/null | head -1)
 if [[ -n "${UDEV_RULES}" ]]; then
     sed -i 's/#MODE="0666"/MODE="0666"/' "${UDEV_RULES}"
     sed -i 's/#GROUP="scanner"/GROUP="scanner"/' "${UDEV_RULES}"
